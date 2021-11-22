d32c198d3d84a2c73d574b7ce00dbea97a09210f
Hernán D. Mercado <hernandmf@gmail.com>
2021-11-22 17:41:08 -0500

    Methods adn statics methods with classes

--- 24-POO/js/01-app.js
+++ 24-POO/js/01-app.js
@@ -3,16 +3,30 @@ class Cliente {
     this.nombre = nombre;
     this.saldo = saldo;
   }
+  mostrarInformacion() {
+    return `Cliente: ${this.nombre}, Saldo: ${this.saldo}`;
+  }
+  static saludo() {
+    return "Bienvenido al cajero";
+  }
 }
 
 const hernan = new Cliente("Hernan", 400);
 console.log(hernan);
+console.log(hernan.mostrarInformacion());
+console.log(Cliente.saludo());
 // console.log("Hola papu");
 
 const Cliente2 = class {
   constructor(nombre, saldo) {
     this.nombre = nombre;
     this.saldo = saldo;
+  }
+  mostrarInformacion() {
+    return `Cliente: ${this.nombre}, Saldo: ${this.saldo}`;
+  }
+  static saludo() {
+    return "Bienvenido al cajero";
   }
 };
 
