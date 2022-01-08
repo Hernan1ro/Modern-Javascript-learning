c05fcba340bbf7961c1b88d09aeb1e8570c05ecc
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-07 23:58:37 -0500

    Pure functions

---
+++ 45-FunctionalJS/js/06-app.js
@@ -0,0 +1,12 @@
+// Pure functions
+
+// reurn the same number of params that the function recives
+const duplicar = (numero) => numero * 2;
+
+const numero1 = 20;
+
+const resultado = duplicar(numero1);
+
+// It does not modify the variable
+console.log(resultado);
+console.log(numero1);
