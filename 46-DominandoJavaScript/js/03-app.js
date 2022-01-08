8015614497034f1f8aa5de7173d60b1a85063a36
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-08 10:18:40 -0500

    coertion

---
+++ 46-DominandoJavaScript/js/03-app.js
@@ -0,0 +1,8 @@
+const numero1 = 20;
+const numero2 = "40";
+// Coercion implicita
+console.log(numero1 + numero2);
+
+// Coerción explicita
+console.log(Number(numero2));
+console.log(numero1.toString());
