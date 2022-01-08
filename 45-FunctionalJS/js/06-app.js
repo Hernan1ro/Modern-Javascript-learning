0abb56b01d111747f8b8bce70654ca047d4884c2
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-08 00:09:55 -0500

    functions that return other functions

--- 45-FunctionalJS/js/06-app.js
+++ 45-FunctionalJS/js/06-app.js
@@ -1,12 +1,12 @@
 // Pure functions
 
 // reurn the same number of params that the function recives
-const duplicar = (numero) => numero * 2;
+const duplicar2 = (numero) => numero * 2;
 
 const numero1 = 20;
 
-const resultado = duplicar(numero1);
+const resultado2 = duplicar2(numero1);
 
 // It does not modify the variable
-console.log(resultado);
+console.log(resultado2);
 console.log(numero1);
---
+++ 45-FunctionalJS/js/07-app.js
@@ -0,0 +1,5 @@
+const obtenerCliente = () => () => console.log("Hernan");
+
+const fn = obtenerCliente();
+
+fn();
