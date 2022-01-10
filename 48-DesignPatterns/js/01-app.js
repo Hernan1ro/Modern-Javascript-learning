074836e032de40aaf27d1fbf76ecd4c6c68a401f
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-09 20:15:56 -0500

    class pattern

---
+++ 48-DesignPatterns/js/01-app.js
@@ -0,0 +1,15 @@
+// Class pattern
+
+// Creation pattern
+
+// Use class to create all objects
+class Persona {
+  constructor(nombre, email) {
+    this.nombre = nombre;
+    this.emial = email;
+  }
+}
+
+const persona = new Persona("Hernan", "Hernandmf@gmail.com");
+
+console.log(persona);
