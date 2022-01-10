3f359fe1dc9f681fffa390c7ad5eef8a5f3cc4ec
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-09 20:33:59 -0500

    singleton pattern

---
+++ 48-DesignPatterns/js/03-app.js
@@ -0,0 +1,22 @@
+// Singleton pattern
+
+// It does not allow to create multiple instances
+
+let instancia = null;
+class Persona {
+  constructor(nombre, email) {
+    if (!instancia) {
+      this.nombre = nombre;
+      this.email = email;
+      instancia = this;
+    } else {
+      return instancia;
+    }
+  }
+}
+
+const persona = new Persona("Hernan", "Hernandmf@gmail.com");
+const persona2 = new Persona("lina", "lina@gmail.com");
+
+console.log(persona);
+console.log(persona2);
