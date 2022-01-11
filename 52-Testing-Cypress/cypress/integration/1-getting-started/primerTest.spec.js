558ddd9a54fce2ed2eae847b12765f2c9aa2c75b
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-11 15:31:10 -0500

    cypress attributes

--- 52-Testing-Cypress/cypress/integration/1-getting-started/primerTest.spec.js
+++ 52-Testing-Cypress/cypress/integration/1-getting-started/primerTest.spec.js
@@ -1,7 +1,21 @@
 describe("Carga la pagina principal", function () {
   it("Carga la página principal", function () {
     cy.visit("http://127.0.0.1:5500/index.html");
-    cy.contains("h1", "Administrador de Pacientes de Veterinaria");
-    cy.get("h1").should("exist");
+
+    // Verificar el elemento y su texto
+    cy.contains(
+      "[data-cy='titulo-proyecto']",
+      "Administrador de Pacientes de Veterinaria"
+    );
+    // Verificar que exista
+    cy.get("[data-cy='titulo-proyecto']").should("exist");
+
+    cy.get('[data-cy="citas-heading"]')
+      .invoke("text")
+      .should("equal", "No hay Citas, comienza creando una");
+    // Verificar que exista el elemento y contenga un texto
+    cy.get("[data-cy='titulo-proyecto']")
+      .invoke("text")
+      .should("equal", "Administrador de Pacientes de Veterinaria");
   });
 });
