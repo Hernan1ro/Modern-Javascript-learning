7313a66d8eeb5d1f1285207987c8d380aee3f764
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-12 00:30:41 -0500

    adding babel to webpack configuration

--- 53-Webpack-ESLint/webpack.config.js
+++ 53-Webpack-ESLint/webpack.config.js
@@ -6,4 +6,18 @@ module.exports = {
     path: path.resolve(__dirname, "public/js"),
     filename: "bundle.js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
 };
