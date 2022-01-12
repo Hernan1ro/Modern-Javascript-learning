21c12f4e7d0203513fb9749bf1ec74ce084cf887
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-11 23:47:38 -0500

    using babel to transform the code

---
+++ 53-Webpack-ESLint/entrada.js
@@ -0,0 +1,10 @@
+class Cliente {
+  constructor(nombre, apellido) {
+    this.nombre = nombre;
+    this.apellid = apellido;
+  }
+}
+
+const hola = () => {
+  console.log("Hola mundo desde babel");
+};
